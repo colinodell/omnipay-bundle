7a3de67a2cbb067beb05567464734015028bcf15
Colin O'Dell <colinodell@gmail.com>
2015-03-19 20:00:08 -0400

    Add missing copyright headers

--- Tests/DependencyInjection/Fixtures/php/methods.php
+++ Tests/DependencyInjection/Fixtures/php/methods.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the colinodell\omnipay-bundle package.
+ *
+ * (c) 2015 Colin O'Dell <colinodell@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $container->loadFromExtension('omnipay', [
     'methods' => [
         'Stripe' => [
